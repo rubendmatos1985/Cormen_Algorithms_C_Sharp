830a65bab45dc6e1ea6e6e1f5603f51405138d19
ruben1985 <rubendmatos1985@gmail.com>
2020-03-07 16:14:17 +0100

    Merge Sort complete

--- Algoritmos_C_Sharp/Program.cs
+++ Algoritmos_C_Sharp/Program.cs
@@ -7,12 +7,14 @@ namespace Algoritmos_C_Sharp
   {
     static void Main(string[] args)
     {
-      int[] arr = new int[] { 7, 9, 1, 24, 12, 0, 99, 5 };
-      int[] sorted = InsertionSort(arr);
-      foreach (int n in sorted)
+      int[] arr = new int[] { 4, 3, 2, 1 };
+
+      MergeSort(arr, 0, arr.Length - 1);
+
+      foreach (int n in arr)
       {
         Console.WriteLine(n);
-        
+
       }
     }
 
@@ -25,12 +27,72 @@ namespace Algoritmos_C_Sharp
         while (i > -1 && arr[i] > numberToInsert)
         {
           arr[i + 1] = arr[i];
-         
+
           i -= 1;
         }
         arr[i + 1] = numberToInsert;
       }
       return arr;
+    }
+
+    static void merge(int[] result, int start, int end)
+    {
+      int middle = (int)Math.Ceiling((double)(end - start + 1) / 2) + start;
+      
+      List<int> L = new List<int>();
+      List<int> R = new List<int>();
+
+      // INSERT VALUES TO LEFT ARRAY
+      for (int i = start; i < middle; i++)
+      {
+        L.Add(result[i]);
+      }
+
+
+      // INSERT VALUES TO RIGHT ARRAY
+      for (int j = middle; j <= end; j++)
+      {
+        R.Add(result[j]);
+      }
+
+      // INSERTING SENTINELS
+      // TO DO -> USE DOUBLES TO INSERT INFINITY
+      L.Add(int.MaxValue);
+      R.Add(int.MaxValue);
+
+
+      int indexLeft = 0;
+      int indexRight = 0;
+
+
+      // MERGE AND SORT ARRRAYS
+      for (int k = start; k <= end; k++)
+      {
+        if (L[indexLeft] <= R[indexRight])
+        {
+          result[k] = L[indexLeft];
+          indexLeft += 1;
+        }
+        else
+        {
+          result[k] = R[indexRight];
+          indexRight += 1;
+        }
+      }
+    }
+
+    // p = arr start 
+    // q = middle  arr 
+    // r = end arr
+    static void MergeSort(int[] arr, int start, int end)
+    {
+      if (start < end)
+      {
+        int middle = (int)Math.Floor((decimal)(start + end) / 2);
+        MergeSort(arr, start, middle);
+        MergeSort(arr, middle + 1, end);
+        merge(arr, start, end);
+      }
     }
 
   }
