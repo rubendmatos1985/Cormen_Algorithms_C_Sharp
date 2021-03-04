3e04e1c38bb27a1ed4a46395ceef4c81e72d0f06
rubendmatos1985 <rubendmatos1985@gmail.com>
2021-03-04 06:34:20 +0100

    unused imports deleted

--- Algoritmos_C_Sharp/BinarySearchTree.cs
+++ Algoritmos_C_Sharp/BinarySearchTree.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 #nullable enable
 namespace Algoritmos_C_Sharp
 {
@@ -89,68 +87,5 @@ namespace Algoritmos_C_Sharp
             WalkTree(_tree);
         }
 
-    }
-
-    class Node
-    {
-        public int Key { get; set; }
-
-        public Node Parent { get; set; }
-
-        public Node Left { get; set; }
-
-        public Node Right { get; set; }
-
-        public Node(int v) => Key = v;
-
-
-        public void Insert(Node value)
-        {
-            value.Parent = this; 
-
-            if (Key > value.Key)
-            {
-
-                if (Left == null)
-                {
-                    Left = value;
-                }
-                else
-                {
-                    Left.Insert(value);
-                }
-            }
-            else
-            {
-                if (Right == null)
-                {
-                    Right = value;
-                }
-                else
-                {
-                    Right.Insert(value);
-                }
-            }
-
-        }
-
-        public override bool Equals(object? obj)
-        {
-           
-            if (obj == null || GetType() != obj.GetType())
-            {
-                return false;
-            }
-
-            
-            return (obj as Node).Key == Key;
-
-        }
-
-        public override int GetHashCode()
-        {
-
-            return (base.GetHashCode() << 2) ^ Key;
-        }
     }
 }
